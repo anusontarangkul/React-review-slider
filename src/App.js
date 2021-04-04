8ea86eacaa1003caec17f7f0b39721d2cefedb5f
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-03 22:48:05 -0700

    slide effect

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
+
   return (
     <section className="section">
       <div className="title">
@@ -35,10 +45,10 @@ function App() {
             <FaQuoteRight className="icon" />
           </article>
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
